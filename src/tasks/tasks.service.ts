7f17bc8cae8b4df4ad609d695c08e07c91a017cb
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-03-29 00:52:59 +0600

    added createTask

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,9 +1,21 @@
+import { v1 as uuid } from 'uuid';
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
 
 @Injectable()
 export class TasksService {
-  private tasks = [];
-  getAllTasks() {
+  private tasks: Task[] = [];
+  getAllTasks(): Task[] {
     return this.tasks;
+  }
+  createTask(title: string, description: string): Task {
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+    this.tasks.push(task);
+    return task;
   }
 }
