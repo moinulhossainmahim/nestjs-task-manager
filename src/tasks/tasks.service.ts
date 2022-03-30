7dffb4c4c339193067fe41221baa4dbeeaf6feb4
moinulhossainmahim <moinulhossainmahim@gmail.com>
2022-03-30 19:15:38 +0600

    added update controller and filter

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -2,12 +2,27 @@ import { v1 as uuid } from 'uuid';
 import { Injectable } from '@nestjs/common';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task-dto';
+import { GetTaskFilterDto } from './dto/get-tasks-filter-dto';
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
   getAllTasks(): Task[] {
     return this.tasks;
+  }
+  getTaskWithFilters(filterDto: GetTaskFilterDto): Task[] {
+    const { status, search } = filterDto;
+    let tasks = this.getAllTasks();
+    if (status) {
+      tasks = tasks.filter((task) => task.status === status);
+    }
+    if (search) {
+      tasks = tasks.filter(
+        (task) =>
+          task.title.includes(search) || task.description.includes(search),
+      );
+    }
+    return tasks;
   }
   getTaskById(id: string): Task {
     return this.tasks.find((task) => task.id === id);
@@ -25,5 +40,10 @@ export class TasksService {
   }
   deletTaskById(id: string): void {
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
   }
 }
